68d7feea38212efa405fd5c500383b7a9aef5016
TomCab98 <tomy.cananillas@gmail.com>
2025-01-22 16:46:25 -0300

    first commit

---
+++ src/main/java/com/projects/task_list_service/TaskListServiceApplication.java
@@ -0,0 +1,13 @@
+package com.projects.task_list_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskListServiceApplication.class, args);
+	}
+
+}
